1cb3883a4dddcb79342cb233835b57a0f780994c
cristianolopes1991 <cristianoizo803javase7@gmail.com>
2024-01-04 08:05:45 -0300

    Ajustes no redis-composer

---
+++ src/main/java/com/poc/demo/PocSpringBootRestRedisApplication.java
@@ -0,0 +1,13 @@
+package com.poc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringBootRestRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringBootRestRedisApplication.class, args);
+	}
+
+}
