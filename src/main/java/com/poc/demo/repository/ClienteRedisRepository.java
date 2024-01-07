23de196f9c75dc9811baf2a058af555e19e0b943
cristianolopes1991 <cristianoizo803javase7@gmail.com>
2024-01-07 13:29:30 -0300

    Inclusão do desenho da solução

--- src/main/java/com/poc/demo/repository/ClienteRedisRepository.java
+++ src/main/java/com/poc/demo/repository/ClienteRedisRepository.java
@@ -2,6 +2,8 @@ package com.poc.demo.repository;
 
 import com.poc.demo.entity.ClienteRedis;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRedisRepository extends CrudRepository <ClienteRedis, String> {
 }
